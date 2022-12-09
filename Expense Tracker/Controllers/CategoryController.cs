eb51e419b53c10210fc971455a5aa16a5dcc6897
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-12-09 10:08:42 +0100

    add-actions-on-categories

--- Expense Tracker/Controllers/CategoryController.cs
+++ Expense Tracker/Controllers/CategoryController.cs
@@ -70,24 +70,6 @@ namespace Expense_Tracker.Controllers
             return View(category);
         }
 
-        // GET: Category/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // POST: Category/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -105,11 +87,6 @@ namespace Expense_Tracker.Controllers
             
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool CategoryExists(int id)
-        {
-          return _context.categories.Any(e => e.CategoryId == id);
         }
     }
 }
